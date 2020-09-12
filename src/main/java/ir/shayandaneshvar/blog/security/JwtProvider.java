5cff03055caa06b854ff76aa0d692230009be330
shayandaneshvar <daneshvarshayan@gmail.com>
2020-09-12 13:34:23 +0430

    replaced Keys from 3rd party jjwt with Java KeyStores

--- src/main/java/ir/shayandaneshvar/blog/security/JwtProvider.java
+++ src/main/java/ir/shayandaneshvar/blog/security/JwtProvider.java
@@ -2,39 +2,71 @@ package ir.shayandaneshvar.blog.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+import ir.shayandaneshvar.blog.exceptions.SpringBlogException;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.security.Key;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 @Component
 public class JwtProvider {
-    private Key key;
+    private KeyStore key;
 
     @PostConstruct
     public void init() {
-        key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        try {
+            key = KeyStore.getInstance("JKS");
+            key.load(getClass().getResourceAsStream("/springblog.jks"),
+                    "123456".toCharArray());
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException |
+                CertificateException e) {
+            e.printStackTrace();
+            throw new SpringBlogException("An error occurred while loading " +
+                    "the keystore");
+        }
     }
 
     public String generateToken(Authentication auth) {
         UserPrincipal principal = (UserPrincipal) auth.getPrincipal();
         return Jwts.builder().setSubject(principal.getUsername())
-                .signWith(key)
+                .signWith(getPrivateKey())
                 .compact();
     }
 
+    private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey) key.getKey("springblog",
+                    "123456".toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException |
+                UnrecoverableKeyException e) {
+            throw new SpringBlogException("Exception occurred while " +
+                    "retrieving the private key from the keystore", e);
+        }
+    }
+
     public boolean validateToken(String jwt) {
-        Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJwt(jwt);
+        Jwts.parserBuilder().setSigningKey(getPublicKey())
+                .build()
+                .parseClaimsJws(jwt);
         return true;
     }
 
+    private PublicKey getPublicKey() {
+        try {
+            return key.getCertificate("springblog").getPublicKey();
+        } catch (KeyStoreException e) {
+            throw new SpringBlogException("Exception occurred while " +
+                    "retrieving the public key from the keystore", e);
+        }
+    }
+
     public String getUsername(String jwt) {
-        Claims claims = Jwts.parserBuilder().setSigningKey(key).build()
-                .parseClaimsJwt(jwt).getBody();
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getPublicKey()).build()
+                .parseClaimsJws(jwt).getBody();
         return claims.getSubject();
     }
 }
